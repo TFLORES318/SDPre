b78ea31ab10395bbb0437454fe99b12d31f934bd
Tabatha <tabatha.flores18@gmail.com>
2020-02-28 22:59:22 -0700

    complete objective2lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,12 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 4.5;
+    num2 = 5.4;
+    num3 = 6.7;
+    average = 5.533333333;
+
+    System.out.println(average);
+  }
+}
