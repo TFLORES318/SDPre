8a8d39a342c6a93d6161e544b5063f3ba5b55d76
Tabatha <tabatha.flores18@gmail.com>
2020-02-28 21:50:17 -0700

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Tabatha Flores";
+    int age = 28;
+    String state = "New York";
+
+
+    System.out.print( "Name: " + name +" ");
+    System.out.print("Age: " + age +" ");
+    System.out.print("State: " + state +" ");
+  }
+}
