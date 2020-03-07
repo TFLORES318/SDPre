ed4813afb01965de28ea4629916354f087205719
Tabatha <tabatha.flores18@gmail.com>
2020-03-06 19:43:45 -0700

    finish jthw ex 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+    public static void main ( String[] args ) {
+        System.out.println( "+------------------------+" );
+        System.out.println( "|                        |" );
+        System.out.println( "|      CORNER STORE      |" );
+        System.out.println( "|                        |" );
+        System.out.println( "| 2020-03-02  05:02PM    |" );
+        System.out.println( "|                        |" );
+        System.out.println( "| Gallons:       11.589  |" );
+        System.out.println( "| Price/gallon: $ 2.609  |" );
+        System.out.println( "|                        |" );
+        System.out.println( "| Fuel total:  $ 30.24   |" );
+        System.out.println( "|                        |" );
+        System.out.println( "+------------------------+" );
+    }
+}
